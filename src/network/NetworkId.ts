9facb6a4cf648537d7cdb31d2516453ed84cc6ae
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-05-14 16:03:26 -0400

    support adding additional network ids

--- src/network/NetworkId.ts
+++ src/network/NetworkId.ts
@@ -1,1 +1,2 @@
-export type NetworkId = 'mainnet' | 'sequence' | 'local'
+export type DefaultNetworkIds = 'mainnet' | 'sequence' | 'local'
+export type NetworkId<T extends string | void = void> = | T extends void ? DefaultNetworkIds : T | DefaultNetworkIds
