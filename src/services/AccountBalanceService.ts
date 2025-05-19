4fe9622ec7bd83bf00818f8fb0211ea5e9b2a72b
Arie Trouw <arie.trouw@xylabs.com>
2025-05-19 14:23:37 -0700

    Added optional bigint values for AccountBalanceService

--- src/services/AccountBalanceService.ts
+++ src/services/AccountBalanceService.ts
@@ -3,6 +3,9 @@ import type {
 } from '@xylabs/hex'
 
 export interface AccountBalanceService {
+  getBalanceValue?: (address: Address) => bigint
+  getBalanceValues?: Record<Address, bigint>
+
   getBalance(address: Address): Hex
   getBalances(): Record<Address, Hex>
   sync(head: Hash): Promise<void>
