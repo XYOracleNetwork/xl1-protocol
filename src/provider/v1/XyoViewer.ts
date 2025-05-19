bd207d1f7acd2bde8a59a04eab921e1c72ee9e7a
Arie Trouw <arie.trouw@xylabs.com>
2025-05-19 14:34:27 -0700

    accountHistory

--- src/provider/v1/XyoViewer.ts
+++ src/provider/v1/XyoViewer.ts
@@ -2,11 +2,13 @@ import type { Address, Hash } from '@xylabs/hex'
 import type { Promisable } from '@xylabs/promise'
 
 import type { Transfer } from '../../payload/index.ts'
-import type { SignedHydratedBlock, SignedHydratedTransaction } from '../../protocol/index.ts'
+import type {
+  SignedHydratedBlock, SignedHydratedTransaction, TransactionBoundWitness,
+} from '../../protocol/index.ts'
 
 export interface XyoViewer {
   accountBalance: (address: Address) => Promisable<bigint>
-  accountHistory: (address: Address) => Promisable<Transfer>
+  accountHistory: (address: Address) => Promisable<[TransactionBoundWitness[], Transfer[]]>
 
   blockByHash(hash: Hash): Promisable<SignedHydratedBlock | null>
   blockByNumber(blockNumber: number): Promisable<SignedHydratedBlock | null>
