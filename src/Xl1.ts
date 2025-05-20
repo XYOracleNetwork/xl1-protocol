02f11a57f319878e40fd266252ba4385c88ce0ea
Arie Trouw <arie.trouw@xylabs.com>
2025-05-20 11:51:38 -0700

    Deploy
    1.3.31

--- src/Xl1.ts
+++ src/Xl1.ts
@@ -4,10 +4,10 @@ export type MicroXL1 = bigint & { readonly _tag: 'MicroXL1' } // 1e-12
 export const XL1 = (value: bigint): XL1 => value as XL1
 export const MicroXL1 = (value: bigint): MicroXL1 => value as MicroXL1
 
-export const mXl1ToXl1 = (value: MicroXL1): XL1 => {
+export const mXL1ToXL1 = (value: MicroXL1): XL1 => {
   return XL1(BigInt(value) / BigInt(1_000_000))
 }
 
-export const xl1ToMxl1 = (value: XL1): MicroXL1 => {
+export const mXL1FromXL1 = (value: XL1): MicroXL1 => {
   return MicroXL1(BigInt(value) * BigInt(1_000_000))
 }
