0ffce9ed58f0c73ee1ac7859921f612a5fd2c78d
Arie Trouw <arie.trouw@xylabs.com>
2025-05-20 16:19:05 -0700

    source map

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,5 +5,6 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
   dts: true,
+  sourcemap: true,
   esbuildPlugins: [TsconfigPathsPlugin({ tsconfig: './tsconfig.json' })],
 })
