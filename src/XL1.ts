65624013deae32af49c36c1f83d93dbbc186f414
Arie Trouw <arie.trouw@xylabs.com>
2025-05-21 13:27:04 -0700

    fixed micro xl1 converters

--- src/XL1.ts
+++ src/XL1.ts
@@ -5,9 +5,9 @@ export const XL1 = (value: bigint): XL1 => value as XL1
 export const MicroXL1 = (value: bigint): MicroXL1 => value as MicroXL1
 
 export const mXL1ToXL1 = (value: MicroXL1): XL1 => {
-  return XL1(BigInt(value) / BigInt(1_000_000))
+  return XL1(BigInt(value) * BigInt(1_000_000))
 }
 
 export const mXL1FromXL1 = (value: XL1): MicroXL1 => {
-  return MicroXL1(BigInt(value) * BigInt(1_000_000))
+  return MicroXL1(BigInt(value) / BigInt(1_000_000))
 }
