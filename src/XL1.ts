7b03a7a589d37c127ad299efc35b37dfa837a41d
Arie Trouw <arie.trouw@xylabs.com>
2025-05-21 13:36:24 -0700

    Deploy
    1.3.39
    Revert XL1 convert change

--- src/XL1.ts
+++ src/XL1.ts
@@ -1,13 +1,13 @@
 export type XL1 = bigint & { readonly _tag: 'XL1' } // 1e-18
-export type MicroXL1 = bigint & { readonly _tag: 'MicroXL1' } // 1e-12
+export type MicroXL1 = bigint & { readonly _tag: 'MicroXL1' } // 1e-24
 
 export const XL1 = (value: bigint): XL1 => value as XL1
 export const MicroXL1 = (value: bigint): MicroXL1 => value as MicroXL1
 
 export const mXL1ToXL1 = (value: MicroXL1): XL1 => {
-  return XL1(BigInt(value) * BigInt(1_000_000))
+  return XL1(BigInt(value) / BigInt(1_000_000))
 }
 
 export const mXL1FromXL1 = (value: XL1): MicroXL1 => {
-  return MicroXL1(BigInt(value) / BigInt(1_000_000))
+  return MicroXL1(BigInt(value) * BigInt(1_000_000))
 }
