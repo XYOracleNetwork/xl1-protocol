7d4b473fa70aa05e556a5d62dcf511f67689a4d6
Arie Trouw <arie.trouw@xylabs.com>
2025-05-27 11:43:34 -0700

    AttoXL1ConvertFactor

--- src/xl1/XL1.ts
+++ src/xl1/XL1.ts
@@ -39,6 +39,21 @@ export const XL1Places: Record<XL1Units, bigint> = {
   atto: 0n,
 } as const
 
+/**
+ * Convert factor by which a respective unit is multiplied to convert it to AttoXL1 or
+ * by which AttoXL1 is divided to convert it to respective unit is multiplied.
+ */
+export const AttoXL1ConvertFactor: Record<XL1Units, bigint> = {
+  xl1: 10n ** XL1Places.xl1,
+  milli: 10n ** XL1Places.milli,
+  micro: 10n ** XL1Places.micro,
+  nano: 10n ** XL1Places.nano,
+  pico: 10n ** XL1Places.pico,
+  femto: 10n ** XL1Places.femto,
+  atto: 10n ** XL1Places.atto,
+} as const
+
+/** @deprecated use AttoXL1ConvertFactor instead */
 export function xl1ConvertFactor(unit: XL1Units) {
   return 10n ** XL1Places[unit]
 }
