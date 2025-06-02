2305c4be0b19caee3ba9e2d69f4118ceb1d98ad9
Arie Trouw <arie.trouw@xylabs.com>
2025-06-02 09:56:55 -0700

    mins back to normal

--- src/constants/minTransactionFees.ts
+++ src/constants/minTransactionFees.ts
@@ -2,8 +2,8 @@ import type { TransactionFeesBigInt } from '../transaction/index.ts'
 import { AttoXL1 } from '../xl1/index.ts'
 
 export const minTransactionFees: TransactionFeesBigInt = {
-  base: AttoXL1(1n),
-  gasPrice: AttoXL1(0n),
-  gasLimit: AttoXL1(0n),
+  base: AttoXL1(1000n),
+  gasPrice: AttoXL1(10n),
+  gasLimit: AttoXL1(1_000_000n),
   priority: AttoXL1(0n),
 } as const
