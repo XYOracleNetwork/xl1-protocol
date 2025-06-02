a016ab8e30af786ef1ed87407326f07c6c4f0909
Arie Trouw <arie.trouw@xylabs.com>
2025-06-02 12:05:11 -0700

    minTransactionFees updated to be atto

--- src/constants/minTransactionFees.ts
+++ src/constants/minTransactionFees.ts
@@ -1,9 +1,9 @@
 import type { TransactionFeesBigInt } from '../transaction/index.ts'
-import { AttoXL1 } from '../xl1/index.ts'
+import { AttoXL1, AttoXL1ConvertFactor } from '../xl1/index.ts'
 
 export const minTransactionFees: TransactionFeesBigInt = {
-  base: AttoXL1(1000n),
-  gasPrice: AttoXL1(10n),
-  gasLimit: AttoXL1(1_000_000n),
-  priority: AttoXL1(0n),
+  base: AttoXL1(1000n * AttoXL1ConvertFactor.micro),
+  gasPrice: AttoXL1(10n * AttoXL1ConvertFactor.micro),
+  gasLimit: AttoXL1(1_000_000n * AttoXL1ConvertFactor.micro),
+  priority: AttoXL1(0n * AttoXL1ConvertFactor.micro),
 } as const
