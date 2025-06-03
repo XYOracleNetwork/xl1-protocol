77630b878925d626acb5137a3d14f3c6a38fa88e
Arie Trouw <arie.trouw@xylabs.com>
2025-06-03 07:24:38 -0700

    Setting fee mins low again as test

--- src/constants/minTransactionFees.ts
+++ src/constants/minTransactionFees.ts
@@ -2,8 +2,8 @@ import type { TransactionFeesBigInt } from '../transaction/index.ts'
 import { AttoXL1, AttoXL1ConvertFactor } from '../xl1/index.ts'
 
 export const minTransactionFees: TransactionFeesBigInt = {
-  base: AttoXL1(1000n * AttoXL1ConvertFactor.micro),
-  gasPrice: AttoXL1(10n * AttoXL1ConvertFactor.micro),
-  gasLimit: AttoXL1(1_000_000n * AttoXL1ConvertFactor.micro),
+  base: AttoXL1(1n), // AttoXL1(1000n * AttoXL1ConvertFactor.micro),
+  gasPrice: AttoXL1(0n), // AttoXL1(10n * AttoXL1ConvertFactor.micro),
+  gasLimit: AttoXL1(0n), // AttoXL1(1_000_000n * AttoXL1ConvertFactor.micro),
   priority: AttoXL1(0n * AttoXL1ConvertFactor.micro),
 } as const
