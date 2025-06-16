53f1ebddafc79d8e3b3f261c495baa5d730ce149
Arie Trouw <arie.trouw@xylabs.com>
2025-06-16 12:26:06 -0700

    statusReporter in BaseServiceParams
    1.4.40

--- src/services/Params.ts
+++ src/services/Params.ts
@@ -3,8 +3,11 @@ import type { EmptyObject } from '@xylabs/object'
 import type { AccountInstance } from '@xyo-network/account-model'
 
 import type { OpenTelemetryProviders } from '../OpenTelemetryProviders.ts'
+import type { ServiceStatusReporter } from './Service.ts'
 
-export type BaseServiceParams<TAdditionalParams extends EmptyObject = EmptyObject> = BaseParams<TAdditionalParams & OpenTelemetryProviders>
+export type BaseServiceParams<TAdditionalParams extends EmptyObject = EmptyObject> = BaseParams<TAdditionalParams & OpenTelemetryProviders & {
+  statusReporter?: ServiceStatusReporter
+}>
 
 export type BaseAccountableServiceParams = BaseServiceParams<{
   account: AccountInstance
