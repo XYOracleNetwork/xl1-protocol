d0c746eb3811fb0f05ed7221882085a6f5c17425
Arie Trouw <arie.trouw@xylabs.com>
2025-06-16 14:55:26 -0700

    name in statusReporter

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -3,7 +3,7 @@ export type ServiceName = Exclude<string, 'reserved-service-name-value'>
 export type ServiceStatus = 'starting' | 'started' | 'stopping' | 'stopped'
 
 export interface ServiceStatusReporter {
-  reportStatus: (status: ServiceStatus, progress?: number) => void
+  reportStatus: (name: ServiceName, status: ServiceStatus, progress?: number) => void
 }
 
 export interface Service {
