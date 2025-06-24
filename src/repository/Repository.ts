809e78322bac85b63de9d74a5dfd579f5504bf2f
Arie Trouw <arie.trouw@xylabs.com>
2025-06-23 17:12:50 -0700

    Deploy
    1.5.6

--- src/repository/Repository.ts
+++ src/repository/Repository.ts
@@ -1,11 +1,25 @@
+import type { Hash } from '@xylabs/hex'
+import type { Promisable } from '@xylabs/promise'
+import type { NextOptions } from '@xyo-network/archivist-model'
+import type { Payload, WithStorageMeta } from '@xyo-network/payload-model'
+
 export interface ReadRepository<TId, TData> {
-  get(id: TId): Promise<TData>
+  get(id: TId): Promisable<TData>
 }
 
 export interface IterableRepository<TId, TData> {
-  next(id: TId): Promise<TData>
+  next(id: TId): Promisable<TData>
 }
 
 export interface WriteRepository<TData, TResp> {
-  insert(data: TData): Promise<TResp>
+  insert(data: TData): Promisable<TResp>
 }
+
+export interface PayloadReadRepository<TId = Hash, TData extends Payload = Payload> extends ReadRepository<TId[], WithStorageMeta<TData>[]> {}
+export interface PayloadWriteRepository<TData extends Payload = Payload> extends WriteRepository<TData[], WithStorageMeta<TData>[]> {}
+
+export interface PayloadIterableRepository<TId = Hash, TData extends Payload = Payload>
+  extends IterableRepository<NextOptions<TId>, WithStorageMeta<TData>[]> {}
+
+export interface PayloadRepository<TId = Hash, TData extends Payload = Payload>
+  extends PayloadReadRepository<TId, TData>, PayloadWriteRepository<TData>, PayloadIterableRepository<TId, TData> {}
