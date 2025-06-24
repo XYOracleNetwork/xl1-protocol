77810baf99690c4c17474bd2978335b3a2b27090
Arie Trouw <arie.trouw@xylabs.com>
2025-06-24 09:52:09 -0700

    AccountBalanceServiceV2

--- src/services/AccountBalanceService.ts
+++ src/services/AccountBalanceService.ts
@@ -1,7 +1,9 @@
 import type {
   Address, Hash, Hex,
 } from '@xylabs/hex'
+import type { Promisable } from '@xylabs/promise'
 
+import type { AttoXL1 } from '../xl1/index.ts'
 import type { Service } from './Service.ts'
 
 export interface AccountBalanceService extends Service {
@@ -11,4 +13,8 @@ export interface AccountBalanceService extends Service {
   getBalance(address: Address): Hex
   getBalances(): Record<Address, Hex>
   sync(head: Hash): Promise<void>
+}
+
+export interface AccountBalanceServiceV2 extends Service {
+  balances(head: Hash, addresses: Address[]): Promisable<Partial<Record<Address, AttoXL1>>>
 }
