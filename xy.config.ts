12da9652eec63595f40039226b73a39dcb80c27d
Arie Trouw <arie.trouw@xylabs.com>
2025-07-11 13:44:09 -0700

    build update

--- xy.config.ts
+++ xy.config.ts
@@ -2,7 +2,7 @@ import type { XyTsupConfig } from '@xylabs/ts-scripts-yarn3'
 const config: XyTsupConfig = {
   compile: {
     browser: {},
-    neutral: { src: { entry: ['src/index.ts', 'src/cli/index.ts', 'src/provider/v2/index.ts'] } },
+    neutral: { src: { entry: ['src/index.ts', 'src/provider/v2/index.ts'] } },
     node: {},
   },
 }
