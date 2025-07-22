ff2915d91ace8bc0260cf8c3c7a61777aa1750bf
Arie Trouw <arie.trouw@xylabs.com>
2025-07-22 12:21:30 -0700

    XyoDataLake update

--- src/provider/v1/XyoDataLake.ts
+++ src/provider/v1/XyoDataLake.ts
@@ -2,23 +2,17 @@ import type { Hash } from '@xylabs/hex'
 import type { Promisable } from '@xylabs/promise'
 import type { Payload } from '@xyo-network/payload-model'
 
-export interface XyoPayloadLakeViewer {
-  getPayloads(hashes: Hash[]): Promisable<Payload[]>
-}
+export type DataLakeData = Payload | ArrayBuffer
 
-export interface XyoPayloadLakeProvider extends XyoPayloadLakeViewer {
-  addPayloads(payloads: Payload[]): Promisable<Hash[]>
-}
-
-export interface XyoDataLakeViewer extends XyoPayloadLakeViewer {
+export interface XyoDataLakeViewer {
   // if resolves to hash payload, keep going until hitting a non-payload or maxDepth, or unable to find
-  fetch(hashes: Hash[], maxDepth?: number): Promisable<(ArrayBuffer | Payload)[]>
+  fetch(hashes: Hash[], maxDepth?: number): Promisable<DataLakeData[]>
   // same as fetch but never follows hash payloads
-  get(hashes: Hash[]): Promisable<(ArrayBuffer | Payload)[]>
-  // same as fetch, except returns each step
-  trace(hashes: Hash[]): Promisable<[(ArrayBuffer | Payload), Payload[]][]>
+  get(hashes: Hash[]): Promisable<DataLakeData[]>
+  // same as fetch, except returns each step in tuple containing the result and the steps
+  trace(hash: Hash): Promisable<[DataLakeData, Payload[]]>
 }
 
-export interface XyoDataLakeProvider extends XyoDataLakeViewer, XyoPayloadLakeProvider {
-  add(items: ArrayBuffer[]): Promisable<Hash[]>
+export interface XyoDataLakeProvider extends XyoDataLakeViewer {
+  add(items: DataLakeData[]): Promisable<DataLakeData[]>
 }
