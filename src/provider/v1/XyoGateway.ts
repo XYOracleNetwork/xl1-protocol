88db402b911eb1aa1068d6e3988e11221e4b32ca
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-07-24 15:34:58 -0500

    Remove permissions from gateway

--- src/provider/v1/XyoGateway.ts
+++ src/provider/v1/XyoGateway.ts
@@ -1,66 +1,11 @@
-/* eslint-disable @typescript-eslint/member-ordering */
-import type { JsonValue } from '@xylabs/object'
 import type { Promisable } from '@xylabs/promise'
 
+import type { InvokerPermission, Permission } from './Permissions.ts'
 import type { TransactionSubmitter } from './TransactionSubmitter.ts'
 import type { XyoConnectionProvider, XyoRpcConnectionConfig } from './XyoConnection.ts'
 import type { XyoSigner } from './XyoSigner.ts'
 
-/**
- * Modeled after EIP-2255
- * See - https://eips.ethereum.org/EIPS/eip-2255#specification
- */
-export interface PermissionRequest {
-  [capability: string]: {
-    [caveatName: string]: JsonValue
-  }
-}
-
-/**
- * Modeled after EIP-2255
- * See - https://eips.ethereum.org/EIPS/eip-2255#specification
- */
-export interface RequestedPermission {
-  /** Permission identifier (i.e. RPC method, action, etc.) */
-  capability: string
-  /** Optional timestamp for when the permission was granted */
-  date?: number
-}
-
-export type CaveatTypes = 'chain' | 'expiration' | 'filteredResponse' | 'rateLimit'
-
-/**
- * Modeled after EIP-2255
- * See - https://eips.ethereum.org/EIPS/eip-2255#specification
- */
-export interface Caveats {
-  /** Type of caveat */
-  type: CaveatTypes
-  /** Value for the caveat (i.e. chain id, subset of accounts, expiration, max request per minute, etc.) */
-  value: JsonValue
-}
-
-/**
- * Modeled after EIP-2255
- * See - https://eips.ethereum.org/EIPS/eip-2255#specification
- */
-export interface Permission {
-  /** Permission identifier (i.e. RPC method, action, etc.) */
-  capability: string
-  /** Caveats for the permission, if applicable (i.e. allowed accounts, signing, etc.) */
-  caveats?: Caveats[]
-  /** Invoker for the given permission (URI, domain, webpage, address, etc.) */
-  invoker: string
-}
-
-export interface InvokerPermission extends Permission {
-  /** Time at which the permission was granted */
-  date?: number
-}
-
 export interface XyoGatewayProvider extends TransactionSubmitter {
-  connection(): Promisable<XyoConnectionProvider>
-  signer(): Promisable<XyoSigner>
 
   /**
    * @deprecated use `connection()` instead
@@ -70,6 +15,10 @@ export interface XyoGatewayProvider extends TransactionSubmitter {
    * @deprecated use `connection()` instead
    */
   addConnection(config: XyoRpcConnectionConfig): Promisable<XyoConnectionProvider>
+  /**
+   * Returns the connection provider for this gateway.
+   */
+  connection(): Promisable<XyoConnectionProvider>
   /**
    * @deprecated use `connection()` instead
    */
@@ -86,4 +35,9 @@ export interface XyoGatewayProvider extends TransactionSubmitter {
    * @deprecated Permissions now at Client level
    */
   revokePermissions(permissions: Permission[]): Promisable<boolean>
+  /**
+   * Returns the signer for this gateway.
+   */
+  signer(): Promisable<XyoSigner>
+
 }
