32971c53d6ccffbaf5328d4aa634f5d06491fdf7
Arie Trouw <arie.trouw@xylabs.com>
2025-08-05 11:02:39 -0700

    NetworkId as GatewayName
    1.9.3

--- src/network/NetworkId.ts
+++ src/network/NetworkId.ts
@@ -1,2 +1,4 @@
-export type DefaultNetworkIds = 'mainnet' | 'sequence' | 'local'
-export type NetworkId<T extends string | void = void> = | T extends void ? DefaultNetworkIds : T | DefaultNetworkIds
+import type { GatewayName } from '../provider/index.ts'
+
+export type DefaultNetworkIds = ('mainnet' & { __gatewayName: true }) | ('sequence' & { __gatewayName: true }) | ('local' & { __gatewayName: true })
+export type NetworkId<T extends GatewayName | void = void> = | T extends void ? DefaultNetworkIds : T | DefaultNetworkIds
