0801ceaedc27c544ac02b843442a6463cd59a1f0
Arie Trouw <arie.trouw@xylabs.com>
2025-08-07 16:06:06 -0700

    isHydratedBoundWitness split

--- src/isHydratedBoundWitness.ts
+++ src/isHydratedBoundWitness.ts
@@ -1,15 +1,25 @@
-import type { HydratedBoundWitnessWithStorageMeta } from '@xyo-network/archivist-model'
-import { isBoundWitnessWithStorageMeta } from '@xyo-network/boundwitness-model'
+import type { HydratedBoundWitness, HydratedBoundWitnessWithStorageMeta } from '@xyo-network/archivist-model'
+import { isBoundWitness } from '@xyo-network/boundwitness-model'
 import { isAnyPayload, isStorageMeta } from '@xyo-network/payload-model'
 
 export const isHydratedBoundWitness = (
   value: unknown,
-): value is HydratedBoundWitnessWithStorageMeta => {
+): value is HydratedBoundWitness => {
   return (
     Array.isArray(value)
     && value.length === 2
-    && isBoundWitnessWithStorageMeta(value[0])
+    && isBoundWitness(value[0])
     && Array.isArray(value[1])
-    && !value[1].some(item => (!isAnyPayload(item) || !isStorageMeta(item)))
+    && !value[1].some(item => (!isAnyPayload(item)))
+  )
+}
+
+export const isHydratedBoundWitnessWithStorageMeta = (
+  value: unknown,
+): value is HydratedBoundWitnessWithStorageMeta => {
+  return (
+    isHydratedBoundWitness(value)
+    && isStorageMeta(value[0])
+    && !value[1].some(item => (!isStorageMeta(item)))
   )
 }
