4dcb6fbfc0d45be77113fdd69ff431700527cdaa
Arie Trouw <arie.trouw@xylabs.com>
2025-08-11 07:47:52 -0700

    undeprecating some is functions

--- src/transaction/TransactionBoundWitnessDeprecated.ts
+++ src/transaction/TransactionBoundWitnessDeprecated.ts
@@ -6,37 +6,28 @@ import { isHashStorageMeta, isStorageMeta } from '@xyo-network/payload-model'
 
 import { isTransactionBoundWitness, type TransactionBoundWitness } from './TransactionBoundWitness.ts'
 
-/** @deprecated use isSigned && isTransactionBoundWitness instead */
 export const isSignedTransactionBoundWitness = (value: unknown): value is Signed<TransactionBoundWitness> => {
   return isTransactionBoundWitness(value) && isSigned(value)
 }
 
-/** @deprecated use isStorageMeta && isTransactionBoundWitness instead */
 export const isTransactionBoundWitnessWithStorageMeta = (value: unknown): value is WithStorageMeta<TransactionBoundWitness> =>
   isTransactionBoundWitness(value)
   && isStorageMeta(value)
 
-/** @deprecated use isHashStorageMeta && isTransactionBoundWitness instead */
 export const isTransactionBoundWitnessWithHashStorageMeta = (value: unknown): value is WithHashStorageMeta<TransactionBoundWitness> =>
   isTransactionBoundWitness(value)
   && isHashStorageMeta(value)
 
-/** @deprecated use isSigned && isStorageMeta && isTransactionBoundWitness instead */
 export const isSignedTransactionBoundWitnessWithStorageMeta = (value: unknown): value is WithStorageMeta<Signed<TransactionBoundWitness>> =>
-  // eslint-disable-next-line sonarjs/deprecation
+
   isSignedTransactionBoundWitness(value)
   && isStorageMeta(value)
 
-/** @deprecated use isSigned && isHashStorageMeta && isTransactionBoundWitness instead */
 export const isSignedTransactionBoundWitnessWithHashStorageMeta = (value: unknown): value is WithHashStorageMeta<Signed<TransactionBoundWitness>> =>
-  // eslint-disable-next-line sonarjs/deprecation
+
   isSignedTransactionBoundWitness(value)
   && isHashStorageMeta(value)
 
-/** @deprecated use asTransactionBoundWitness && asStorageMeta instead */
-// eslint-disable-next-line sonarjs/deprecation
 export const asTransactionBoundWitnessWithStorageMeta = AsObjectFactory.create(isTransactionBoundWitnessWithStorageMeta)
 
-/** @deprecated use asTransactionBoundWitness && asHashStorageMeta instead */
-// eslint-disable-next-line sonarjs/deprecation
 export const asTransactionBoundWitnessWithHashStorageMeta = AsObjectFactory.create(isTransactionBoundWitnessWithHashStorageMeta)
