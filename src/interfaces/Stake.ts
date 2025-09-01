7ed0c1037ec684c6879bad78e570d57bf8e92adb
Arie Trouw <arie.trouw@xylabs.com>
2025-09-01 13:37:01 -0700

    Amount in Stake object changed from bigint to Hex for serialization

--- src/interfaces/Stake.ts
+++ src/interfaces/Stake.ts
@@ -1,11 +1,11 @@
-import type { Address } from '@xylabs/hex'
+import type { Address, Hex } from '@xylabs/hex'
 import type { Promisable } from '@xylabs/promise'
 
 export type Stake = {
   // the block number when the stake was added
   addBlock: number
   // the amount that is staked
-  amount: bigint
+  amount: Hex
   // the unique id for the stake item
   id: number
   // the block number when the stake was removed (set to 0 if not removed)
