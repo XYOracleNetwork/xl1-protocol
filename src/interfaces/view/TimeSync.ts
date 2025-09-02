762b0a4e9bb7248ef2f4232ef2600b3346439575
Arie Trouw <arie.trouw@xylabs.com>
2025-09-02 14:56:30 -0700

    Updated TimeSync interface

--- src/interfaces/view/TimeSync.ts
+++ src/interfaces/view/TimeSync.ts
@@ -3,6 +3,6 @@ import type { Promisable } from '@xylabs/promise'
 import type { TimeDomain } from '../../payload/index.ts'
 
 export interface TimeSyncViewInterface {
-  convertTime: (from: TimeDomain, to: TimeDomain) => Promisable<number>
+  convertTime: (fromDomain: TimeDomain, toDomain: TimeDomain, from: number) => Promisable<number>
   currentTime: (domain: TimeDomain) => Promisable<number>
 }
