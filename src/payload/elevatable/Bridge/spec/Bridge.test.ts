2188edb70cd289ae86ca3004d836647833832698
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-05 13:56:48 -0500

    Sample test for bridge intent

---
+++ src/payload/elevatable/Bridge/spec/Bridge.test.ts
@@ -0,0 +1,39 @@
+import { toHex } from '@xylabs/hex'
+import {
+  describe, expect, it,
+} from 'vitest'
+
+import type { Chain } from '../../../../model.ts'
+import { AttoXL1ConvertFactor } from '../../../../xl1/index.ts'
+import type { BridgeIntentFields } from '../BridgeIntent.ts'
+
+describe('Bridge', () => {
+  const amount = toHex(18n * AttoXL1ConvertFactor.xl1) // 18 XL1 in AttoXL1
+
+  const xl1ChainId: Chain = toHex('dd381fbb392c85160d8b0453e446757b12384046')
+  const ethChainId = toHex('0x01')
+
+  const xl1Sender = toHex('0xabc123')
+  const ethReceiver = toHex('0xabc123')
+
+  const bridgeableTokenContract = toHex('0xF5d1C979ce9D2CAf07c247771b195A2FBec8a9b4')
+
+  const nonce = 'd5eeff33-d5bc-4aca-9ecb-3406c02a5dc4'
+
+  it('BridgeIntent', () => {
+    const intent: BridgeIntentFields = {
+      amount, // TODO: Should source & dest token amounts be different?
+      dest: ethChainId, // To Ethereum
+      destAddress: ethReceiver,
+      destToken: bridgeableTokenContract,
+      direction: 'srcToDest', // TODO: Implied
+      initiator: xl1Sender, // TODO: In BW or Servicer Module Address
+      nonce,
+      src: xl1ChainId, // From XL1
+      srcAddress: xl1Sender, // From XL1 sender
+      srcToken: xl1ChainId, // In XL1
+    }
+
+    expect(intent).toMatchSnapshot()
+  })
+})
