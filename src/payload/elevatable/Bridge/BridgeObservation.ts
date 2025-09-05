31db88dc1ea12f676945d304887b29a4cedf7b7e
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-05 17:50:48 -0500

    Allow for source or dest confirmation

--- src/payload/elevatable/Bridge/BridgeObservation.ts
+++ src/payload/elevatable/Bridge/BridgeObservation.ts
@@ -3,16 +3,25 @@ import type { Hex } from '@xylabs/hex'
 import type { BridgeDetailsFields } from './BridgeDetails.ts'
 
 /**
+ * Represents an observation that confirms a bridge action occurred on the source chain.
+ */
+export interface BridgeSourceObservationFields extends BridgeDetailsFields {
+  /**
+   * Source chain confirmation
+   */
+  srcConfirmation?: Hex
+}
+/**
  * Represents an observation that confirms a bridge action occurred on the destination chain.
  */
-export interface BridgeObservationFields extends BridgeDetailsFields {
+export interface BridgeDestinationObservationFields extends BridgeDetailsFields {
   /**
    * Destination chain confirmation
    */
-  destConfirmation: Hex
-
-  /**
-   * Type of confirmation (e.g., 'txHash', 'eventId')
-   */
-  destConfirmationType?: string
+  destConfirmation?: Hex
 }
+
+/**
+ * Represents an observation that confirms a bridge action occurred on either the source or destination chain.
+ */
+export type BridgeObservationFields = BridgeSourceObservationFields | BridgeDestinationObservationFields
