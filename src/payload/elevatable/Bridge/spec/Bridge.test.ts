57147b6002bdb5a48ff14f60d2d24ac099d2f82b
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-05 17:23:17 -0500

    Use simplified values in tests

--- src/payload/elevatable/Bridge/spec/Bridge.test.ts
+++ src/payload/elevatable/Bridge/spec/Bridge.test.ts
@@ -1,4 +1,4 @@
-import { toHex } from '@xylabs/hex'
+import { toAddress, toHex } from '@xylabs/hex'
 import {
   describe, expect, it,
 } from 'vitest'
@@ -6,6 +6,7 @@ import {
 import type { Chain } from '../../../../model.ts'
 import { AttoXL1ConvertFactor } from '../../../../xl1/index.ts'
 import type { BridgeIntentFields } from '../BridgeIntent.ts'
+import type { BridgeObservationFields } from '../BridgeObservation.ts'
 
 describe('Bridge', () => {
   const srcAmount = toHex(18n * AttoXL1ConvertFactor.xl1) // 18 XL1 in AttoXL1
@@ -14,26 +15,43 @@ describe('Bridge', () => {
   const xl1ChainId: Chain = toHex('dd381fbb392c85160d8b0453e446757b12384046')
   const ethChainId = toHex('0x01')
 
-  const xl1Sender = toHex('0xabc123')
-  const ethReceiver = toHex('0xabc123')
+  const xl1Sender = toAddress('1111111111111111111111111111111111111111')
+  const ethReceiver = toAddress('0x2222222222222222222222222222222222222222')
 
-  const bridgeableTokenContract = toHex('0xF5d1C979ce9D2CAf07c247771b195A2FBec8a9b4')
+  const bridgeableTokenContract = toHex('0x3333333333333333333333333333333333333333')
 
   const nonce = 'd5eeff33-d5bc-4aca-9ecb-3406c02a5dc4'
 
-  it('BridgeIntent', () => {
-    const intent: BridgeIntentFields = {
-      dest: ethChainId, // To Ethereum
-      destAddress: ethReceiver,
-      destAmount,
-      destToken: bridgeableTokenContract,
-      nonce,
-      src: xl1ChainId, // From XL1
-      srcAddress: xl1Sender, // From XL1 sender
-      srcAmount,
-      srcToken: xl1ChainId, // In XL1
-    }
+  const ethTxConfirmationHash = toHex('0x4444444444444444444444444444444444444444444444444444444444444444') // Some Eth tx hash
 
-    expect(intent).toMatchSnapshot()
+  describe('Bridge to ETH', () => {
+    it('BridgeIntent', () => {
+      const intent: BridgeIntentFields = {
+        dest: ethChainId, // To Ethereum
+        destAddress: ethReceiver,
+        destAmount,
+        destToken: bridgeableTokenContract,
+        nonce,
+        src: xl1ChainId, // From XL1
+        srcAddress: xl1Sender, // From XL1 sender
+        srcAmount,
+        srcToken: xl1ChainId, // In XL1
+      }
+      expect(intent).toMatchSnapshot()
+    })
+    it('BridgeObservation', () => {
+      const observation: BridgeObservationFields = {
+        dest: ethChainId, // To Ethereum
+        destAddress: ethReceiver,
+        destAmount,
+        destToken: bridgeableTokenContract,
+        destConfirmation: ethTxConfirmationHash,
+        src: xl1ChainId, // From XL1
+        srcAddress: xl1Sender, // From XL1 sender
+        srcAmount,
+        srcToken: xl1ChainId, // In XL1
+      }
+      expect(observation).toMatchSnapshot()
+    })
   })
 })
