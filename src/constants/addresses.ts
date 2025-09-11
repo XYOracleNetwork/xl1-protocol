a363f2a181da31fa9697d1860895d28dda1a749d
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-11 15:40:25 -0500

    Documentation

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -5,8 +5,6 @@ import type { Address } from '@xylabs/hex'
  */
 export const XYO_ZERO_ADDRESS = '0000000000000000000000000000000000000000' as const as Address
 
-export const XYO_STEP_REWARD_ADDRESS = '1216197612161976121619761216197612161976' as const as Address
-
 /**
  * The Bridge Address is used as a marker address for bridge transactions.
  */
@@ -16,3 +14,8 @@ export const XYO_BRIDGE_ADDRESS = '3012193230121932301219323012193230121932' as 
  * The Network Staking Address is used as a marker address for staking the network.
  */
 export const XYO_NETWORK_STAKING_ADDRESS = '1969196919691969196919691969196919691969' as const as Address
+
+/**
+ * The Step Reward Address is used as a marker address for step rewards.
+ */
+export const XYO_STEP_REWARD_ADDRESS = '1216197612161976121619761216197612161976' as const as Address
