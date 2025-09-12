6f5830d0dd2a6b854addbf70743de6b5c4b95d1f
Arie Trouw <arie.trouw@xylabs.com>
2025-09-13 10:58:36 +1200

    Adding Hashes to Time Payload

--- src/payload/elevatable/Time.ts
+++ src/payload/elevatable/Time.ts
@@ -1,3 +1,4 @@
+import type { Hash } from '@xylabs/hex'
 import { AsObjectFactory } from '@xylabs/object'
 import type { Payload } from '@xyo-network/payload-model'
 import { isPayloadOfSchemaType } from '@xyo-network/payload-model'
@@ -8,18 +9,35 @@ export type TimeDomain = 'xl1' | 'epoch' | 'ethereum'
 export const TimeSchema = 'network.xyo.time' as const
 export type TimeSchema = typeof TimeSchema
 
-export interface TimeFields {
+export interface XL1TimeFields {
+  // block number
+  xl1?: number
+  // block hash
+  xl1Hash?: Hash
+}
+
+export interface EthereumTimeFields {
+  // block number
+  ethereum?: number
+  // block hash
+  ethereumHash?: Hash
+}
+
+export interface TimeFields extends XL1TimeFields, EthereumTimeFields {
   // in milliseconds
   epoch: number
-  // in block number
-  ethereum?: number
-  // in block number
-  xl1?: number
 }
 
 export type TimePayload = Payload<TimeFields, TimeSchema>
 
-export const isTimePayload = isPayloadOfSchemaType<TimePayload>(TimeSchema)
+// to prevent scaling problems, we use double the current time as a max safe epoch
+export const isSafeEpoch = (value: unknown): value is number => {
+  return typeof value === 'number' && value < 2 * Date.now()
+}
+
+export const isTimePayload = (value: unknown): value is TimePayload => {
+  return isPayloadOfSchemaType<TimePayload>(TimeSchema)(value) && isSafeEpoch(value.epoch)
+}
 
 export const asTimePayload = AsObjectFactory.create(isTimePayload)
 export const asTimePayloadWithStorageMeta = AsObjectFactory.create(isTimePayload)
