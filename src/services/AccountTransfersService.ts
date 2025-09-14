8b9ea7843e853e7ee9fd7646cc65252dd79f95a6
Arie Trouw <arie.trouw@xylabs.com>
2025-09-15 10:56:50 +1200

    Deploy
    1.12.31

--- src/services/AccountTransfersService.ts
+++ src/services/AccountTransfersService.ts
@@ -5,8 +5,8 @@ import type { AttoXL1 } from '../xl1/index.ts'
 import type { ServiceInterface } from './Service.ts'
 
 export interface AccountTransfersProvider {
-  transfer(head: Hash, from: Address, to: Address): Promisable<AttoXL1>
-  transfers(head: Hash, addresses: Address[]): Promisable<Partial<Record<Address, AttoXL1>>>
+  transfer(head: Hash, account: Address): Promisable<AttoXL1>
+  transfers(head: Hash, accounts: Address[]): Promisable<Partial<Record<Address, AttoXL1>>>
 }
 
 export interface AccountTransfersService extends AccountTransfersProvider, ServiceInterface {}
