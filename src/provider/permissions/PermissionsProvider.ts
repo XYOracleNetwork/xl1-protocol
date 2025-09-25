68f4845c5f8c5b5c3ca88679d5008147299a7499
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-09-25 11:05:47 -0400

    revert shape change to keep compatibility

--- src/provider/permissions/PermissionsProvider.ts
+++ src/provider/permissions/PermissionsProvider.ts
@@ -3,7 +3,7 @@ import type { Promisable } from '@xylabs/promise'
 import type { InvokerPermission, Permission } from './InvokerPermission.ts'
 
 export interface PermissionsProvider {
-  permissions(): Promisable<InvokerPermission[]>
-  request(permissions: Permission[]): Promisable<void>
-  revoke(permissions: Permission[]): Promisable<void>
+  getPermissions(): Promisable<InvokerPermission[]>
+  requestPermissions(permissions: Permission[]): Promisable<boolean>
+  revokePermissions(permissions: Permission[]): Promisable<boolean>
 }
