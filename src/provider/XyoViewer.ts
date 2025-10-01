06ef956bc911c2b0121d76713637cd58caf155bf
Arie Trouw <arie.trouw@xylabs.com>
2025-10-01 12:46:00 -0700

    NetworkStakeStepRewardViewInterface in XyoViewer

--- src/provider/XyoViewer.ts
+++ src/provider/XyoViewer.ts
@@ -1,9 +1,10 @@
 import type {
   AccountBalanceViewInterface, BlockViewInterface, ChainViewInterface,
   ForkViewInterface,
+  NetworkStakeStepRewardViewInterface,
   StakeViewInterface,
   TransactionViewInterface, TransferBalanceViewInterface,
 } from '../interfaces/index.ts'
 
-export interface XyoViewer extends AccountBalanceViewInterface, TransferBalanceViewInterface,
+export interface XyoViewer extends AccountBalanceViewInterface, TransferBalanceViewInterface, NetworkStakeStepRewardViewInterface,
   ChainViewInterface, BlockViewInterface, TransactionViewInterface, StakeViewInterface, ForkViewInterface {}
