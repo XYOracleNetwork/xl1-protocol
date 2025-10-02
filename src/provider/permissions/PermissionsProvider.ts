2e8aad8c353ee369beaf004cdb51e4bae96e395c
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-10-02 15:31:35 -0500

    Update permissions interfaces

--- src/provider/permissions/PermissionsProvider.ts
+++ src/provider/permissions/PermissionsProvider.ts
@@ -1,9 +1,9 @@
 import type { Promisable } from '@xylabs/promise'
 
-import type { InvokerPermission, Permission } from './InvokerPermission.ts'
+import type { InvokerPermission, RequestedPermission } from './InvokerPermission.ts'
 
 export interface PermissionsProvider {
-  getPermissions(): Promisable<InvokerPermission[]>
-  requestPermissions(permissions: Permission[]): Promisable<boolean>
-  revokePermissions(permissions: Permission[]): Promisable<boolean>
+  getPermissions(permissions: RequestedPermission[]): Promisable<InvokerPermission[]>
+  requestPermissions(permissions: RequestedPermission[]): Promisable<boolean>
+  revokePermissions(permissions: RequestedPermission[]): Promisable<boolean>
 }
