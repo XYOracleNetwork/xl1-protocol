feb202383db477048cc386b4c77cb1c40517b32f
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-10-02 17:14:19 -0400

    PermissionsProvider to XyoPermissions

--- src/provider/permissions/XyoPermissions.ts
+++ src/provider/permissions/XyoPermissions.ts
@@ -2,8 +2,11 @@ import type { Promisable } from '@xylabs/promise'
 
 import type { InvokerPermission, RequestedPermission } from './InvokerPermission.ts'
 
-export interface PermissionsProvider {
+export interface XyoPermissions {
   getPermissions(permissions: RequestedPermission[]): Promisable<InvokerPermission[]>
   requestPermissions(permissions: RequestedPermission[]): Promisable<boolean>
   revokePermissions(permissions: RequestedPermission[]): Promisable<boolean>
 }
+
+// @deprecated - use XyoPermissions instead
+export interface PermissionsProvider extends XyoPermissions {}
