1ba74109a44fcb93230f1ff2e01b9f5c3df869f5
Arie Trouw <arie.trouw@xylabs.com>
2025-10-03 16:48:55 -0700

    Removed deprecated fields from XyoGateway

--- src/provider/XyoGateway.ts
+++ src/provider/XyoGateway.ts
@@ -1,8 +1,5 @@
 import type { Promisable } from '@xylabs/promise'
 
-import type { InvokerPermission, Permission } from './permissions/index.ts'
-// eslint-disable-next-line sonarjs/deprecation
-import type { TransactionSubmitter } from './TransactionSubmitter.ts'
 import type { XyoConnection } from './XyoConnection.ts'
 import type { XyoGatewayHelpers } from './XyoGatewayHelpers.ts'
 import type { XyoSigner } from './XyoSigner.ts'
@@ -12,38 +9,7 @@ export interface XyoConnectionConfigDeprecated {
   name: string
 }
 
-/** @deprecated use XyoGatewayProvider instead */
-// eslint-disable-next-line sonarjs/deprecation
-export interface XyoGatewayProviderDeprecated extends TransactionSubmitter {
-  /**
-   * @deprecated use `connection()` instead
-   */
-  activeConnection(): Promisable<XyoConnection | undefined>
-  /**
-   * @deprecated use `connection()` instead
-   */
-  // eslint-disable-next-line sonarjs/deprecation
-  addConnection(config: XyoConnectionConfigDeprecated): Promisable<XyoConnection>
-  /**
-   * @deprecated use `connection()` instead
-   */
-  connections(): Promisable<Record<string, XyoConnection>>
-  /**
-   * @deprecated Permissions now at Client level
-   */
-  getPermissions(): Promisable<InvokerPermission[]>
-  /**
-   * @deprecated Permissions now at Client level
-   */
-  requestPermissions(permissions: Permission[]): Promisable<boolean>
-  /**
-   * @deprecated Permissions now at Client level
-   */
-  revokePermissions(permissions: Permission[]): Promisable<boolean>
-}
-
-// eslint-disable-next-line sonarjs/deprecation
-export interface XyoGatewayProvider extends Partial<XyoGatewayHelpers>, Partial<XyoGatewayProviderDeprecated> {
+export interface XyoGatewayProvider extends Partial<XyoGatewayHelpers> {
   /**
    * Returns the connection provider for this gateway.
    */
