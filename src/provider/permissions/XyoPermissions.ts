1759b4e173145860f60057047b9bcbc7327cd5f4
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-10-03 09:47:05 -0400

    make interface params and return closely match spec

--- src/provider/permissions/XyoPermissions.ts
+++ src/provider/permissions/XyoPermissions.ts
@@ -1,11 +1,23 @@
 import type { Promisable } from '@xylabs/promise'
 
-import type { InvokerPermission, RequestedPermission } from './InvokerPermission.ts'
+import type {
+  InvokerPermission, PermissionRequest, RequestedPermission,
+} from './InvokerPermission.ts'
 
 export interface XyoPermissions {
-  getPermissions(permissions: RequestedPermission[]): Promisable<InvokerPermission[]>
-  requestPermissions(permissions: RequestedPermission[]): Promisable<boolean>
-  revokePermissions(permissions: RequestedPermission[]): Promisable<boolean>
+  /**
+   * Returns the permissions that are currently granted
+   *
+   * Per the spec - "The wallet_getPermissions method is used for getting an array of current permissions
+   * (empty by default). It takes no parameters and returns an array of Permission objects."
+   *
+   * See - https://eips.ethereum.org/EIPS/eip-2255#specification
+   */
+  getPermissions(): Promisable<InvokerPermission[]>
+  // Given a permissions request, attempts to request the permissions from the user
+  requestPermissions(permissions: PermissionRequest[]): Promisable<RequestedPermission[]>
+  // Given a permissions request, attempts to revoke the permissions from the user
+  revokePermissions(permissions: PermissionRequest): Promisable<RequestedPermission[]>
 }
 
 // @deprecated - use XyoPermissions instead
