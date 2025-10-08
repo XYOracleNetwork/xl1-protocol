2214bbace80e78d17c98a29fe5301a3c6a5e291c
Arie Trouw <arie.trouw@xylabs.com>
2025-10-08 15:27:29 -0700

    Updated NetworkStakeStepRewardViewInterface

--- src/interfaces/view/NetworkStakeStepReward.ts
+++ src/interfaces/view/NetworkStakeStepReward.ts
@@ -1,39 +1,40 @@
 import type { Address } from '@xylabs/hex'
 import type { Promisable } from '@xylabs/promise'
 
+export interface StepContext {
+  block: number
+  step: number
+}
+
 export interface NetworkStakeStepAddressRewardViewInterface {
-  networkStakeClaimedRewards(address: Address): Promisable<bigint>
-
-  // the step rewards for a specific network stakers for a given step and block
-  networkStakeStepAddressReward(address: Address, step: number, block: number): Promisable<Record<Address, bigint>>
-
   // the step rewards for a specific network stakers for all of history
   networkStakeStepRewardAddressHistory(address: Address): Promisable<Record<Address, bigint>>
 
+  // the step rewards for a specific network stakers for a given step and block
+  networkStakeStepRewardAddressReward(context: StepContext, address: Address): Promisable<Record<Address, bigint>>
+
   // the shares for a specific network staker for a given step and block
-  networkStakeStepRewardAddressShare(address: Address, step: number, block: number): Promisable<[/* address shares */bigint, /* total shares */bigint]>
+  networkStakeStepRewardAddressShare(context: StepContext, address: Address): Promisable<[/* address shares */bigint, /* total shares */bigint]>
 
   // estimate the current reward weight for a given address at a given step
-  networkStakeStepRewardWeightForAddress(address: Address, step: number): Promisable<bigint>
-
-  networkStakeUnclaimedRewards(address: Address): Promisable<bigint>
+  networkStakeStepRewardWeightForAddress(context: StepContext, address: Address): Promisable<bigint>
 }
 
 export interface NetworkStakeStepPoolRewardViewInterface {
 
-  // all the shares for all the network stakers for a given step and block
-  networkStakeStepPoolRewardShares(step: number, block: number): Promisable<Record<Address, bigint>>
-
   // all the step rewards for all the network stakers for a given step and block
-  networkStakeStepPoolRewards(step: number, block: number): Promisable<Record<Address, bigint>>
+  networkStakeStepRewardPoolRewards(context: StepContext): Promisable<Record<Address, bigint>>
+
+  // all the shares for all the network stakers for a given step and block
+  networkStakeStepRewardPoolShares(context: StepContext): Promisable<Record<Address, bigint>>
 }
 
 export interface NetworkStakeStepRewardPositionViewInterface {
   // estimate the current weight for a given position at a given step
-  networkStakeStepRewardPositionWeight(position: bigint, step: number): Promisable<number>
+  networkStakeStepRewardPositionWeight(context: StepContext, position: number): Promisable<number>
 
   // estimate the potential loss for removing a given position at a given step
-  networkStakeStepRewardPotentialPositionLoss(position: bigint, step: number): Promisable<bigint>
+  networkStakeStepRewardPotentialPositionLoss(context: StepContext, position: number): Promisable<bigint>
 }
 
 export interface NetworkStakeStepRewardViewInterface extends
@@ -41,12 +42,16 @@ export interface NetworkStakeStepRewardViewInterface extends
   NetworkStakeStepAddressRewardViewInterface,
   NetworkStakeStepRewardPositionViewInterface
 {
+  networkStakeStepRewardClaimedByAddress(address: Address): Promisable<bigint>
+
   // estimate the total possible block rewards for a given step
-  networkStakeStepRewardForStep(step: number): Promisable<bigint>
+  networkStakeStepRewardForStep(context: StepContext): Promisable<bigint>
 
   // the predictable random number for a given step and block
-  networkStakeStepRewardRandomizer(step: number, block: number): Promisable<bigint>
+  networkStakeStepRewardRandomizer(context: StepContext): Promisable<bigint>
 
   // the total number of stakers for a given step and block
-  networkStakeStepRewardStakerCount(step: number, block: number): Promisable<number>
+  networkStakeStepRewardStakerCount(context: StepContext): Promisable<number>
+
+  networkStakeStepRewardUnclaimedByAddress(address: Address): Promisable<bigint>
 }
