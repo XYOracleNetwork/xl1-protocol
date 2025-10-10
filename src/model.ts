9a9dbc3edba88d587ddf412b9fea310c11ee7c50
Arie Trouw <arie.trouw@xylabs.com>
2025-10-10 16:42:58 -0700

    1.12.63
    ChainId

--- src/model.ts
+++ src/model.ts
@@ -2,10 +2,10 @@ import type {
   Address, Hash, Hex,
 } from '@xylabs/hex'
 
-export type ChainId = Address
+export type ChainId = Address | Hex
 
 /** @deprecated use ChainId instead */
-export type Chain = ChainId | Hex
+export type Chain = ChainId
 
 export type CompletedStep = [
   /* Hash of the block whose previous hash is the last item in the step */
