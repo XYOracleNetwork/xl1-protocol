bae09c31cdc61af450094f4cb502862c0cc28f97
Arie Trouw <arie.trouw@xylabs.com>
2025-10-14 15:43:48 -0700

    Stake amount type

--- src/interfaces/view/Stake.ts
+++ src/interfaces/view/Stake.ts
@@ -1,13 +1,11 @@
 import type { Address } from '@xylabs/hex'
 import type { Promisable } from '@xylabs/promise'
 
-import type { AttoXL1 } from '../../xl1/index.ts'
-
 export type Stake = {
   // the block number when the stake was added
   addBlock: number
   // the amount that is staked
-  amount: AttoXL1
+  amount: bigint
   // the unique id for the stake item
   id: number
   // the block number when the stake was removed (set to 0 if not removed)
