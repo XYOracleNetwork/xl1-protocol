a106e243843167b1ccd219a2b3e681cc993e1535
Arie Trouw <arie.trouw@xylabs.com>
2025-10-20 12:24:29 -0700

    Added context to Transfer payload

--- src/payload/elevatable/TransferPayload.ts
+++ src/payload/elevatable/TransferPayload.ts
@@ -11,14 +11,15 @@ import type { FromFields } from './Executable.ts'
 export const TransferSchema = 'network.xyo.transfer' as const
 export type TransferSchema = typeof TransferSchema
 
-export interface TransferFields extends FromFields {
+export interface TransferFields<TContext extends {} = {}> extends FromFields {
+  context?: TContext
   epoch: number
   // the amount that is being sent to other addresses
   transfers: Partial<Record<Address, Hex>>
 }
 
 // if this payload is included in a boundwitness, it needs to be available for inspection to be included in block
-export type Transfer = Payload<TransferFields, TransferSchema>
+export type Transfer<TContext extends {} = {}> = Payload<TransferFields<TContext>, TransferSchema>
 
 export const isTransfer = isPayloadOfSchemaType<Transfer>(TransferSchema)
 
