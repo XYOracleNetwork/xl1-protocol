b13de5cf85476d8c7ebc8b2490cdd72c9971d2ab
Arie Trouw <arie.trouw@xylabs.com>
2025-10-20 12:01:21 -0700

    updo and context on hydrated tx validation

--- src/validation/transaction/HydratedTransactionValidationFunction.ts
+++ src/validation/transaction/HydratedTransactionValidationFunction.ts
@@ -1,16 +1,15 @@
 import type { Promisable } from '@xylabs/promise'
 
-import type { ChainId } from '../../model/index.ts'
 import type { HydratedTransactionWithStorageMeta, TransactionBoundWitness } from '../../transaction/index.ts'
 import type { HydratedTransactionValidationError } from './error.ts'
 
 /**
  * A function that validates a hydrated transaction.
  * @param bw The hydrated transaction to validate.
- * @param chainId The chain ID to use for validation.
+ * @param context The context to use for validation.
  * @returns An array of errors if the transaction is invalid, or an empty array if it is valid.
  */
-export type HydratedTransactionValidationFunction<T extends TransactionBoundWitness = TransactionBoundWitness> = (
+export type HydratedTransactionValidationFunction<T extends TransactionBoundWitness = TransactionBoundWitness, TContext extends {} = {}> = (
   hydratedTransaction: HydratedTransactionWithStorageMeta<T>,
-  chainId?: ChainId,
+  context?: TContext
 ) => Promisable<HydratedTransactionValidationError[]>
