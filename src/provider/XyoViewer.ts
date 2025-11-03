42ccb151e5840e86c474c9fb99104e3435934c71
Arie Trouw <arie.trouw@xylabs.com>
2025-11-03 13:20:59 -0800

    XyoViewerMethods
    1.12.100

--- src/provider/XyoViewer.ts
+++ src/provider/XyoViewer.ts
@@ -7,8 +7,11 @@ import type {
   TransactionViewInterface, TransferBalanceViewInterface,
 } from '../interfaces/index.ts'
 
-export interface XyoViewer extends AccountBalanceViewInterface, TransferBalanceViewInterface,
+export interface XyoViewerMethods extends AccountBalanceViewInterface, TransferBalanceViewInterface,
   NetworkStakeStepRewardViewInterface,
   ChainViewInterface, BlockViewInterface, TransactionViewInterface, StakeViewer, ForkViewer {
+}
+
+export interface XyoViewer extends XyoViewerMethods {
   networkStakeViewer?: NetworkStakeViewer
 }
