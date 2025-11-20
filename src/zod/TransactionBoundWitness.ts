3ba103c9b5e370907b37528c5ed7ef10465263bf
Arie Trouw <arie.trouw@xylabs.com>
2025-11-19 20:01:53 -0800

    Deploy
    1.14.3

--- src/zod/TransactionBoundWitness.ts
+++ src/zod/TransactionBoundWitness.ts
@@ -2,6 +2,7 @@ import { AddressZod } from '@xylabs/sdk-js'
 import {
   zodAsFactory, zodIsFactory, zodToFactory,
 } from '@xylabs/zod'
+import { HashMetaZod, StorageMetaZod } from '@xyo-network/payload-model'
 import * as z from 'zod'
 
 import { BlockNumberZod } from '../model/index.ts'
@@ -32,6 +33,24 @@ export const isTransactionBoundWitness = zodIsFactory(TransactionBoundWitnessZod
 export const asTransactionBoundWitness = zodAsFactory(TransactionBoundWitnessZod, 'asTransactionBoundWitness')
 export const toTransactionBoundWitness = zodToFactory(TransactionBoundWitnessZod, 'toTransactionBoundWitness')
 
+export const TransactionBoundWitnessWithHashMetaZod = TransactionBoundWitnessZod
+  .extend(HashMetaZod.shape)
+
+export type TransactionBoundWitnessWithHashMeta = z.infer<typeof TransactionBoundWitnessWithHashMetaZod>
+
+export const isTransactionBoundWitnessWithHashMeta = zodIsFactory(TransactionBoundWitnessWithHashMetaZod)
+export const asTransactionBoundWitnessWithHashMeta = zodAsFactory(TransactionBoundWitnessWithHashMetaZod, 'asTransactionBoundWitnessWithHashMeta')
+export const toTransactionBoundWitnessWithHashMeta = zodToFactory(TransactionBoundWitnessWithHashMetaZod, 'toTransactionBoundWitnessWithHashMeta')
+
+export const TransactionBoundWitnessWithStorageMetaZod = TransactionBoundWitnessZod
+  .extend(StorageMetaZod.shape)
+
+export type TransactionBoundWitnessWithStorageMeta = z.infer<typeof TransactionBoundWitnessWithStorageMetaZod>
+
+export const isTransactionBoundWitnessWithStorageMeta = zodIsFactory(TransactionBoundWitnessWithStorageMetaZod)
+export const asTransactionBoundWitnessWithStorageMeta = zodAsFactory(TransactionBoundWitnessWithStorageMetaZod, 'asTransactionBoundWitnessWithStorageMeta')
+export const toTransactionBoundWitnessWithStorageMeta = zodToFactory(TransactionBoundWitnessWithStorageMetaZod, 'toTransactionBoundWitnessWithStorageMeta')
+
 export const UnsignedTransactionBoundWitnessZod = UnsignedBoundWitnessZod
   .extend(BlockDurationZod.shape)
   .extend(TransactionFeesZod.shape)
@@ -44,6 +63,35 @@ export const isUnsignedTransactionBoundWitness = zodIsFactory(UnsignedTransactio
 export const asUnsignedTransactionBoundWitness = zodAsFactory(UnsignedTransactionBoundWitnessZod, 'asUnsignedTransactionBoundWitness')
 export const toUnsignedTransactionBoundWitness = zodToFactory(UnsignedTransactionBoundWitnessZod, 'toUnsignedTransactionBoundWitness')
 
+export const UnsignedTransactionBoundWitnessWithHashMetaZod = UnsignedTransactionBoundWitnessZod.extend(HashMetaZod.shape)
+
+export type UnsignedTransactionBoundWitnessWithHashMeta = z.infer<typeof UnsignedTransactionBoundWitnessWithHashMetaZod>
+
+export const isUnsignedTransactionBoundWitnessWithHashMeta = zodIsFactory(UnsignedTransactionBoundWitnessWithHashMetaZod)
+export const asUnsignedTransactionBoundWitnessWithHashMeta = zodAsFactory(
+  UnsignedTransactionBoundWitnessWithHashMetaZod,
+  'asUnsignedTransactionBoundWitnessWithHashMeta',
+)
+export const toUnsignedTransactionBoundWitnessWithHashMeta = zodToFactory(
+  UnsignedTransactionBoundWitnessWithHashMetaZod,
+  'toUnsignedTransactionBoundWitnessWithHashMeta',
+)
+
+export const UnsignedTransactionBoundWitnessWithStorageMetaZod = UnsignedTransactionBoundWitnessZod
+  .extend(StorageMetaZod.shape)
+
+export type UnsignedTransactionBoundWitnessWithStorageMeta = z.infer<typeof UnsignedTransactionBoundWitnessWithStorageMetaZod>
+
+export const isUnsignedTransactionBoundWitnessWithStorageMeta = zodIsFactory(UnsignedTransactionBoundWitnessWithStorageMetaZod)
+export const asUnsignedTransactionBoundWitnessWithStorageMeta = zodAsFactory(
+  UnsignedTransactionBoundWitnessWithStorageMetaZod,
+  'asUnsignedTransactionBoundWitnessWithStorageMeta',
+)
+export const toUnsignedTransactionBoundWitnessWithStorageMeta = zodToFactory(
+  UnsignedTransactionBoundWitnessWithStorageMetaZod,
+  'toUnsignedTransactionBoundWitnessWithStorageMeta',
+)
+
 export const SignedTransactionBoundWitnessZod = SignedBoundWitnessZod
   .extend(BlockDurationZod.shape)
   .extend(TransactionFeesZod.shape)
@@ -55,3 +103,33 @@ export type SignedTransactionBoundWitness = z.infer<typeof SignedTransactionBoun
 export const isSignedTransactionBoundWitness = zodIsFactory(SignedTransactionBoundWitnessZod)
 export const asSignedTransactionBoundWitness = zodAsFactory(SignedTransactionBoundWitnessZod, 'asSignedTransactionBoundWitness')
 export const toSignedTransactionBoundWitness = zodToFactory(SignedTransactionBoundWitnessZod, 'toSignedTransactionBoundWitness')
+
+export const SignedTransactionBoundWitnessWithHashMetaZod = SignedTransactionBoundWitnessZod
+  .extend(HashMetaZod.shape)
+
+export type SignedTransactionBoundWitnessWithHashMeta = z.infer<typeof SignedTransactionBoundWitnessWithHashMetaZod>
+
+export const isSignedTransactionBoundWitnessWithHashMeta = zodIsFactory(SignedTransactionBoundWitnessWithHashMetaZod)
+export const asSignedTransactionBoundWitnessWithHashMeta = zodAsFactory(
+  SignedTransactionBoundWitnessWithHashMetaZod,
+  'asSignedTransactionBoundWitnessWithHashMeta',
+)
+export const toSignedTransactionBoundWitnessWithHashMeta = zodToFactory(
+  SignedTransactionBoundWitnessWithHashMetaZod,
+  'toSignedTransactionBoundWitnessWithHashMeta',
+)
+
+export const SignedTransactionBoundWitnessWithStorageMetaZod = SignedTransactionBoundWitnessZod
+  .extend(StorageMetaZod.shape)
+
+export type SignedTransactionBoundWitnessWithStorageMeta = z.infer<typeof SignedTransactionBoundWitnessWithStorageMetaZod>
+
+export const isSignedTransactionBoundWitnessWithStorageMeta = zodIsFactory(SignedTransactionBoundWitnessWithStorageMetaZod)
+export const asSignedTransactionBoundWitnessWithStorageMeta = zodAsFactory(
+  SignedTransactionBoundWitnessWithStorageMetaZod,
+  'asSignedTransactionBoundWitnessWithStorageMeta',
+)
+export const toSignedTransactionBoundWitnessWithStorageMeta = zodToFactory(
+  SignedTransactionBoundWitnessWithStorageMetaZod,
+  'toSignedTransactionBoundWitnessWithStorageMeta',
+)
