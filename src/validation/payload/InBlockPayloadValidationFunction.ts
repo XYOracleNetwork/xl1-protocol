8edf427bc1be30b0aa631406af77dddbf236e5f0
Arie Trouw <arie.trouw@xylabs.com>
2025-11-20 09:41:39 -0800

    Deploy
    1.14.8

--- src/validation/payload/InBlockPayloadValidationFunction.ts
+++ src/validation/payload/InBlockPayloadValidationFunction.ts
@@ -1,9 +1,9 @@
 import type { Promisable } from '@xylabs/promise'
 import type { Payload, WithHashMeta } from '@xyo-network/payload-model'
 
-import type { HydratedBlock } from '../../zod/index.ts'
+import type { HydratedBlockWithHashMeta } from '../../zod/index.ts'
 import type { InBlockPayloadValidationError } from './error.ts'
 
 export type InBlockPayloadValidationFunction = (
-  payload: WithHashMeta<Payload>, block: HydratedBlock,
+  payload: WithHashMeta<Payload>, block: HydratedBlockWithHashMeta,
 ) => Promisable<InBlockPayloadValidationError[]>
