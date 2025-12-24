0261f679c1d28038c132dc9299aa5df8299bebc3
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-12-24 10:44:36 -0500

    update description

--- packages/protocol/src/zod/BlockRate.ts
+++ packages/protocol/src/zod/BlockRate.ts
@@ -5,9 +5,9 @@ import { XL1BlockRangeZod } from '../model/index.ts'
 
 export const BlockRateZod = z.object({
   range: XL1BlockRangeZod.describe('the block range the rate was calculated over'),
-  rate: z.number().nonnegative().describe('number of milliseconds to make a block'),
+  rate: z.number().nonnegative().describe('time to make a block'),
   span: z.int().nonnegative().describe('number of blocks the rate was calculated over'),
-  timeDifference: z.number().nonnegative().describe('time difference in milliseconds for start and end block'),
+  timeDifference: z.number().nonnegative().describe('time difference from start and end block'),
 })
 
 export type BlockRateZod = z.infer<typeof BlockRateZod>
