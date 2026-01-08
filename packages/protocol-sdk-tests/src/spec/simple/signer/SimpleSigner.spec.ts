e8a285a6ebc75819be2341b9ded9df0bc935da67
Matt Jones <885417+jonesmac@users.noreply.github.com>
2026-01-07 21:52:58 -0500

    spec file for SimpleSigner

---
+++ packages/protocol-sdk-tests/src/spec/simple/signer/SimpleSigner.spec.ts
@@ -0,0 +1,52 @@
+import { Account } from '@xyo-network/account'
+import type { AccountInstance } from '@xyo-network/account-model'
+import type { Payload } from '@xyo-network/payload-model'
+import type { AllowedBlockPayload, ChainId } from '@xyo-network/xl1-protocol'
+import {
+  asXL1BlockNumber, defaultTransactionFees, SignedHydratedTransactionWithHashMetaZod,
+} from '@xyo-network/xl1-protocol'
+import type { XyoSigner } from '@xyo-network/xl1-protocol-sdk'
+import {
+  buildUnsignedTransaction, SimpleXyoSigner, XyoSignerMoniker,
+} from '@xyo-network/xl1-protocol-sdk'
+import { buildProviderLocator } from '@xyo-network/xl1-providers'
+import {
+  beforeEach, describe, expect, it,
+} from 'vitest'
+
+describe('SimpleXyoSigner', () => {
+  let sut: XyoSigner
+  let account: AccountInstance
+
+  beforeEach(async () => {
+    account = await Account.random()
+    const locator = buildProviderLocator()
+    locator.register(SimpleXyoSigner.factory<SimpleXyoSigner>(SimpleXyoSigner.dependencies, { account }))
+    sut = await locator.getInstance<SimpleXyoSigner>(XyoSignerMoniker)
+  })
+
+  it('should create an instance of SimpleXyoSigner', () => {
+    expect(sut).toBeInstanceOf(SimpleXyoSigner)
+  })
+
+  it('should expose the the signer address', () => {
+    expect(sut.address()).toBe(account.address)
+  })
+
+  it('should sign a transaction', async () => {
+    const chain = '2AAE728aFd1777b79c34D79c4523797F9D9965b0' as ChainId
+    const elevatedPayloads: AllowedBlockPayload[] = []
+    const additionalPayloads: Payload[] = []
+    const nbf = asXL1BlockNumber(100_000, true)
+    const exp = asXL1BlockNumber(200_000, true)
+    const fees = defaultTransactionFees
+
+    const unsignedTransaction = await buildUnsignedTransaction(chain, elevatedPayloads, additionalPayloads, nbf, exp, account.address, fees)
+
+    const signedTx = await sut.signTransaction(unsignedTransaction)
+
+    expect(signedTx).toBeDefined()
+    const result = SignedHydratedTransactionWithHashMetaZod.safeParse(signedTx)
+    expect(result.success).toBe(true)
+  })
+})
