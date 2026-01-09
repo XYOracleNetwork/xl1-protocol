42117e1872bc0ce8b0ed9415133e83b41374ba58
Joel Carter <JoelBCarter@users.noreply.github.com>
2026-01-09 14:46:11 -0600

    Use locator for test

--- packages/protocol-sdk-tests/src/spec/simple/mempool/SimpleMempoolViewer.spec.ts
+++ packages/protocol-sdk-tests/src/spec/simple/mempool/SimpleMempoolViewer.spec.ts
@@ -2,12 +2,20 @@ import '@xylabs/vitest-extended'
 
 import { MemoryArchivist } from '@xyo-network/archivist-memory'
 import { SimpleMempoolViewer } from '@xyo-network/xl1-protocol-sdk'
-import { buildProviderLocator } from '@xyo-network/xl1-providers'
+import { buildSimpleProviderLocator } from '@xyo-network/xl1-providers'
 import {
   beforeAll, beforeEach, describe, expect, it,
 } from 'vitest'
 
 describe.skip('SimpleMempoolViewer', () => {
+  async function buildSimpleMempoolViewer(pendingBlocksArchivist: MemoryArchivist, pendingTransactionsArchivist: MemoryArchivist) {
+    const locator = buildSimpleProviderLocator()
+    locator.register(
+      SimpleMempoolViewer.factory<SimpleMempoolViewer>(SimpleMempoolViewer.dependencies, { pendingBlocksArchivist, pendingTransactionsArchivist }),
+    )
+    return await locator.getInstance<SimpleMempoolViewer>(SimpleMempoolViewer.defaultMoniker)
+  }
+
   let sut: SimpleMempoolViewer
   let pendingBlocksArchivist: MemoryArchivist
   let pendingTransactionsArchivist: MemoryArchivist
@@ -15,11 +23,7 @@ describe.skip('SimpleMempoolViewer', () => {
   beforeAll(async () => {
     pendingBlocksArchivist = await MemoryArchivist.create({ account: 'random' })
     pendingTransactionsArchivist = await MemoryArchivist.create({ account: 'random' })
-    const locator = buildProviderLocator()
-    sut = await SimpleMempoolViewer.create({
-      pendingBlocksArchivist,
-      pendingTransactionsArchivist,
-    })
+    sut = await buildSimpleMempoolViewer(pendingBlocksArchivist, pendingTransactionsArchivist)
   })
 
   beforeEach(async () => {
