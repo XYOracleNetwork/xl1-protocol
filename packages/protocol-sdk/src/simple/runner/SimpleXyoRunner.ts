7906033b409f90223eb3aa61dccd53852d27bf2d
Joel Carter <JoelBCarter@users.noreply.github.com>
2026-01-16 17:25:28 -0600

    Prevent duplicate insertion of transactions into mempool

--- packages/protocol-sdk/src/simple/runner/SimpleXyoRunner.ts
+++ packages/protocol-sdk/src/simple/runner/SimpleXyoRunner.ts
@@ -1,19 +1,13 @@
-import {
-  assertEx,
-  type Hash,
-} from '@xylabs/sdk-js'
-import type { ArchivistInstance } from '@xyo-network/archivist-model'
+import { type Hash } from '@xylabs/sdk-js'
 import { PayloadBuilder } from '@xyo-network/payload-builder'
 import type { SignedHydratedTransactionWithStorageMeta } from '@xyo-network/xl1-protocol'
 
 import type { CreatableProviderParams } from '../../CreatableProvider/index.ts'
 import { AbstractCreatableProvider, creatableProvider } from '../../CreatableProvider/index.ts'
-import { hydratedTransactionToPayloadBundle } from '../../model/index.ts'
 import { type XyoRunner, XyoRunnerMoniker } from '../../provider/index.ts'
 import { type MempoolRunner, MempoolRunnerMoniker } from '../../runners/index.ts'
 
 export interface SimpleXyoRunnerParams extends CreatableProviderParams {
-  pendingTransactionsArchivist: ArchivistInstance
 }
 
 @creatableProvider()
@@ -29,20 +23,7 @@ export class SimpleXyoRunner extends AbstractCreatableProvider<SimpleXyoRunnerPa
     return this._mempoolRunner!
   }
 
-  static override async paramsHandler(params?: SimpleXyoRunnerParams) {
-    return {
-      ...await super.paramsHandler(params),
-      pendingTransactionsArchivist: assertEx(
-        params?.pendingTransactionsArchivist,
-        () => 'A pendingTransactionsArchivist is required to create a SimpleXyoRunner',
-      ),
-    }
-  }
-
   async broadcastTransaction(transaction: SignedHydratedTransactionWithStorageMeta): Promise<Hash> {
-    const archivist = this.params.pendingTransactionsArchivist
-    const bundle = hydratedTransactionToPayloadBundle(transaction)
-    await archivist.insert([bundle])
     await this.mempool.submitTransactions([transaction])
     const hash = await PayloadBuilder.hash(transaction[0])
     this.logger?.info(`Broadcasted transaction with hash ${hash}`)
