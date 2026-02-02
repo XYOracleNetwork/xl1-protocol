752212730e66ee755de486269ec4c2f8c15e3712
Arie Trouw <arie.trouw@xylabs.com>
2026-02-02 08:06:31 -0800

    Deploy
    1.22.1

--- packages/protocol/src/validation/payload/InBlockPayloadValidationFunction.ts
+++ packages/protocol/src/validation/payload/InBlockPayloadValidationFunction.ts
@@ -1,9 +1,15 @@
 import type { Promisable } from '@xylabs/sdk-js'
 import type { Payload, WithHashMeta } from '@xyo-network/payload-model'
 
+import type { BaseContext, ChainId } from '../../model/index.ts'
 import type { HydratedBlockWithHashMeta } from '../../zod/index.ts'
 import type { InBlockPayloadValidationError } from './error.ts'
 
+export interface InBlockPayloadValidationFunctionContext extends BaseContext {
+  chainId: ChainId
+}
+
 export type InBlockPayloadValidationFunction = (
+  context: InBlockPayloadValidationFunctionContext,
   payload: WithHashMeta<Payload>, block: HydratedBlockWithHashMeta,
 ) => Promisable<InBlockPayloadValidationError[]>
