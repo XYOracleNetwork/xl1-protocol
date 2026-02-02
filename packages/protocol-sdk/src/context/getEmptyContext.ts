8941534058a7f73b25381cf5f9aefd2e9793592e
Arie Trouw <arie.trouw@xylabs.com>
2026-02-02 11:22:57 -0800

    getEmptyContext

---
+++ packages/protocol-sdk/src/context/getEmptyContext.ts
@@ -0,0 +1,13 @@
+import type { Config } from '../config/index.ts'
+import { getDefaultConfig } from '../config/index.ts'
+import { type CreatableProviderContext, ProviderFactoryLocator } from '../CreatableProvider/index.ts'
+
+export function getEmptyContext(config?: Config): CreatableProviderContext {
+  const resolvedConfig = config ?? getDefaultConfig()
+  const singletons = {}
+  const caches = {}
+  const locator = new ProviderFactoryLocator({
+    config: resolvedConfig, singletons, caches,
+  })
+  return locator.context
+}
