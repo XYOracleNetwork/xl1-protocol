5165f9691953a5bfa409a64e55ac159d20903ab7
Arie Trouw <arie.trouw@xylabs.com>
2026-02-02 16:55:40 -0800

    BaseContext telemetry changes

--- packages/protocol/src/model/BaseContext.ts
+++ packages/protocol/src/model/BaseContext.ts
@@ -1,4 +1,4 @@
-import type { Tracer } from '@opentelemetry/api'
+import type { MeterProvider, TracerProvider } from '@opentelemetry/api'
 import type { Logger } from '@xylabs/sdk-js'
 
 import type { MapType } from '../map/index.ts'
@@ -6,7 +6,8 @@ import type { MapType } from '../map/index.ts'
 export interface BaseContext<TCacheValue = string | object | number | bigint> {
   caches?: Record<string, MapType<string, TCacheValue>>
   logger?: Logger
+  meterProvider?: MeterProvider
   singletons: Record<string, unknown>
   timeBudgetLimit?: number
-  tracer?: Tracer
+  traceProvider?: TracerProvider
 }
