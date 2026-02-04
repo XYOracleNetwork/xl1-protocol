f49a3e3f2886bb8d86c0df8b38cc9c3ec8bdf4bf
Arie Trouw <arie.trouw@xylabs.com>
2026-02-04 13:30:20 -0800

    stepsRewardTotalGenesisPeriod context type update

--- packages/protocol-sdk/src/primitives/chain/step/stepsRewardTotal.ts
+++ packages/protocol-sdk/src/primitives/chain/step/stepsRewardTotal.ts
@@ -1,5 +1,5 @@
 import type {
-  AttoXL1, BlockViewer, ChainContext, StepIdentity, XL1BlockRange,
+  AttoXL1, BlockViewer, CachingContext, StepIdentity, XL1BlockRange,
   XL1RangeMultipliers,
 } from '@xyo-network/xl1-protocol'
 import { asAttoXL1, asXL1BlockRange } from '@xyo-network/xl1-protocol'
@@ -8,13 +8,13 @@ import { XL1_NETWORK_STAKING_GENESIS_PERIOD_END_XL1_BLOCK } from '../../../const
 import { blockRangeSteps } from '../../block/index.ts'
 import { stepRewardTotal } from './stepRewardTotal.ts'
 
-export async function stepsRewardTotalGenesisPeriod(context: ChainContext, blockViewer: BlockViewer, multipliers: XL1RangeMultipliers, stepSizes?: number[]): Promise<AttoXL1> {
+export async function stepsRewardTotalGenesisPeriod(context: CachingContext, blockViewer: BlockViewer, multipliers: XL1RangeMultipliers, stepSizes?: number[]): Promise<AttoXL1> {
   const range = asXL1BlockRange([0, XL1_NETWORK_STAKING_GENESIS_PERIOD_END_XL1_BLOCK], { name: 'stepsRewardTotalGenesisPeriod' })
   return await stepsRewardTotalRange(context, blockViewer, range, stepSizes, multipliers)
 }
 
 export async function stepsRewardTotalRange(
-  context: ChainContext,
+  context: CachingContext,
   blockViewer: BlockViewer,
   range: XL1BlockRange,
   stepSizes: number[] = [3, 4, 5, 6],
@@ -24,7 +24,7 @@ export async function stepsRewardTotalRange(
   return await stepsRewardTotal(context, blockViewer, steps, multipliers)
 }
 
-export async function stepsRewardTotal(context: ChainContext, blockViewer: BlockViewer, steps: StepIdentity[], multipliers: XL1RangeMultipliers): Promise<AttoXL1> {
+export async function stepsRewardTotal(context: CachingContext, blockViewer: BlockViewer, steps: StepIdentity[], multipliers: XL1RangeMultipliers): Promise<AttoXL1> {
   let totalRewards = 0n
   for (const step of steps) {
     const stepTotal = await stepRewardTotal(context, blockViewer, step, multipliers)
