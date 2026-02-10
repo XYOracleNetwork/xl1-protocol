c35a8b47a7cba9f45dfd6c442c6d9895fdf3e684
Joel Carter <JoelBCarter@users.noreply.github.com>
2026-02-10 15:02:56 -0600

    Update rpc path in test

--- packages/rpc/src/transport/spec/HttpRpcTransport.spec.ts
+++ packages/rpc/src/transport/spec/HttpRpcTransport.spec.ts
@@ -7,7 +7,7 @@ import { HttpRpcTransport } from '../HttpRpcTransport.ts'
 
 describe.runIf(false)('HttpRpcTransport', () => {
   it('should send a request and receive a response', async () => {
-    const sut = new HttpRpcTransport('http://localhost:8080', XyoViewerRpcSchemas, true)
+    const sut = new HttpRpcTransport('http://localhost:8080/rpc', XyoViewerRpcSchemas, true)
     const result = await sut.sendRequest('xyoViewer_currentBlock')
     expect(result).toBeDefined()
   })
