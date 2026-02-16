595576224330315d489f0704eeb4116f399f4a74
Arie Trouw <arie.trouw@xylabs.com>
2026-02-16 14:09:12 -0800

    Deploy
    1.24.12

--- packages/protocol/src/providers/DataLake/Runner.ts
+++ packages/protocol/src/providers/DataLake/Runner.ts
@@ -2,10 +2,11 @@ import type { Hash } from '@xylabs/sdk-js'
 import type { WriteArchivistFunctions } from '@xyo-network/archivist-model'
 
 import type { DataLakeData, DataLakeProvider } from './Provider.ts'
+import type { DataLakeViewer, DataLakeViewerMethods } from './Viewer.ts'
 
 export const DataLakeRunnerMoniker = 'DataLakeRunner' as const
 export type DataLakeRunnerMoniker = typeof DataLakeRunnerMoniker
 
-export interface DataLakeRunnerMethods extends WriteArchivistFunctions<DataLakeData, DataLakeData, DataLakeData, Hash> {}
+export interface DataLakeRunnerMethods extends WriteArchivistFunctions<DataLakeData, DataLakeData, DataLakeData, Hash>, DataLakeViewerMethods {}
 
-export interface DataLakeRunner extends DataLakeProvider<DataLakeRunnerMoniker>, DataLakeRunnerMethods {}
+export interface DataLakeRunner extends DataLakeProvider<DataLakeRunnerMoniker>, DataLakeRunnerMethods, Omit<DataLakeViewer, 'moniker'> {}
