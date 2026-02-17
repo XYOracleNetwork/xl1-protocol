9d20bd9ab0c6fc668eca5c95fb0ddbed06fc92a1
Arie Trouw <arie.trouw@xylabs.com>
2026-02-16 17:55:20 -0800

    removed statusReporter from ActorContext

--- packages/protocol-sdk/src/context/Actor.ts
+++ packages/protocol-sdk/src/context/Actor.ts
@@ -1,15 +1,12 @@
-import type { CreatableStatusReporter } from '@xylabs/sdk-js'
 import {
   zodAsFactory, zodIsFactory, zodToFactory,
 } from '@xylabs/zod'
-import { z } from 'zod'
+import type { z } from 'zod'
 
 import { ActorConfigZod } from '../config/index.ts'
 import { BaseConfigContextZod } from '../model/index.ts'
 
-const StatusReporterInstanceZod = z.custom<CreatableStatusReporter>(val => val && typeof val === 'object' && 'report' in val)
-
-export const ActorConfigContext = BaseConfigContextZod.extend({ config: ActorConfigZod, statusReporter: StatusReporterInstanceZod.optional() })
+export const ActorConfigContext = BaseConfigContextZod.extend({ config: ActorConfigZod })
 
 export type ActorConfigContext = z.infer<typeof ActorConfigContext>
 
