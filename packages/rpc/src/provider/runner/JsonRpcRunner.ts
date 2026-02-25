d219221223fe2e8c0c9a9d6133c4fc365c5459a2
Arie Trouw <arie.trouw@xylabs.com>
2026-02-25 13:56:28 -0800

    PostMessageRpcTransport creatable from config

--- packages/rpc/src/provider/runner/JsonRpcRunner.ts
+++ packages/rpc/src/provider/runner/JsonRpcRunner.ts
@@ -1,7 +1,11 @@
 import type { CreatableProviderParams } from '@xyo-network/xl1-protocol-sdk'
-import { AbstractCreatableProvider, HttpRpcRemoteConfigZod } from '@xyo-network/xl1-protocol-sdk'
+import {
+  AbstractCreatableProvider, HttpRpcRemoteConfigZod, PostMessageRpcRemoteConfigZod,
+} from '@xyo-network/xl1-protocol-sdk'
 
-import { HttpRpcTransport, type RpcTransport } from '../../transport/index.ts'
+import {
+  HttpRpcTransport, PostMessageRpcTransport, type RpcTransport,
+} from '../../transport/index.ts'
 import type { RpcSchemaMap } from '../../types/index.ts'
 
 export interface JsonRpcRunnerParams<TSchemas extends RpcSchemaMap = RpcSchemaMap> extends CreatableProviderParams {
@@ -24,6 +28,12 @@ export abstract class AbstractJsonRpcRunner<TSchemas extends RpcSchemaMap,
     if (httpRemoteConfig.success) {
       const { url } = httpRemoteConfig.data.rpc
       return new HttpRpcTransport(url, this.schemas())
+    }
+
+    const postMessageRemoteConfig = PostMessageRpcRemoteConfigZod.safeParse(this.config.remote)
+    if (postMessageRemoteConfig.success) {
+      const { networkId, sessionId } = postMessageRemoteConfig.data.rpc
+      return new PostMessageRpcTransport(networkId, this.schemas(), sessionId, this.logger)
     }
 
     throw new Error('Unable to create transport')
