7d5b9fbac678f4559fca37396fe1eede9e167fce
Arie Trouw <arie.trouw@xylabs.com>
2026-03-02 08:42:29 -0800

    deprecations in XyoClient

--- packages/protocol/src/providers/XyoClient.ts
+++ packages/protocol/src/providers/XyoClient.ts
@@ -3,10 +3,12 @@ import type { XyoPermissions } from '../permissions/index.ts'
 import type { XyoGateway } from './XyoGateway.ts'
 import type { XyoGatewayRunner } from './XyoGatewayRunner.ts'
 
+/** @deprecated */
 export interface XyoGatewayConfig {
   name: GatewayName
 }
 
+/** @deprecated */
 export interface XyoRpcGatewayConfig extends XyoGatewayConfig {
   dataLakeEndpoint: string
   networkEndpoint: string
@@ -16,6 +18,8 @@ export interface XyoClient {
   gateways: Readonly<Partial<Record<GatewayName, XyoGatewayRunner>>>
   permissions: XyoPermissions
 
+  /** @deprecated */
   addGateway?(providerOrConfig: XyoGatewayConfig | XyoRpcGatewayConfig | XyoGateway): Promise<XyoGateway>
+  /** @deprecated */
   removeGateway?(name: GatewayName): Promise<void>
 }
